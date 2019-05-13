e790b30f09eb2badaebbf86e4d6764918769f3aa
zhou199611 <zhou199019901@163.com>
2019-05-13 11:07:14 +0800

    excel merge

--- example5.py
+++
@@ -1,9 +0,0 @@
-import pyprind
-import time
-
-n = 50
-bar = pyprind.ProgBar(n, monitor=True)
-for i in range(50):
-    time.sleep(0.15)
-    bar.update()
-print(bar)
--- example6.py
+++
@@ -1,8 +0,0 @@
-import pyprind
-import time
-n = 50
-bar = pyprind.ProgPercent(n, monitor=True)
-for i in range(50):
-    time.sleep(0.15)
-    bar.update()
-print(bar)
